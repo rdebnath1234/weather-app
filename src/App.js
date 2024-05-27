df7420ea50cc4ba297f7bea1a361eb2e85da9ff9
Your Name <you@example.com>
2024-05-27 14:36:16 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("mumbai");
+  const [weatherData, setWeatherData] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/weather/?city=${city}`)
+      .then((res) => {
+        setWeatherData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [city]);
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <input type="text" value={city} onChange={handleCityChange} placeholder="Enter city" />
+      <button>Search</button>
+      {
+        weatherData &&
+        <div>
+          <h2>{weatherData.city}</h2>
+        </div>
+      }
     </div>
   );
 }
